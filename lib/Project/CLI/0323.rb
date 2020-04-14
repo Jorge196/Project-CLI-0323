f02090506a3e546913e44efd505772f4ef74bac8
Jorge Ramirez <joramirez96@gmail.com>
2020-04-14 20:16:24 +0000

    first commit

---
+++ lib/Project/CLI/0323.rb
@@ -0,0 +1,10 @@
+require "Project/CLI/0323/version"
+
+module Project
+  module CLI
+    module 0323
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/Project/CLI/0323/version.rb
@@ -0,0 +1,7 @@
+module Project
+  module CLI
+    module 0323
+      VERSION = "0.1.0"
+    end
+  end
+end
