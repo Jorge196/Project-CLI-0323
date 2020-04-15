10fe226ecb36b44da9af52f666df2f8a8f32554e
Jorge Ramirez <joramirez96@gmail.com>
2020-04-15 19:41:09 +0000

    test commit

---
+++ lib/enviornment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'HTTParty'
+require 'json'
+
+require_relative './pokedex/cli'
+require_relative './pokedex/api'
+require_relative './pokedex/pokemon'
+require_relative './pokedex/abilities'
---
+++ lib/pokedex/cli.rb
@@ -0,0 +1,8 @@
+class Cli
+  def run
+    puts " "
+    puts "Hello Trainer, and welcome to the POKEDEX!"
+    puts " "
+    # puts "Enter a Pokemon to see its "
+  end
+end
