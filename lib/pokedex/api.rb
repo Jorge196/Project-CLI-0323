aa64b6764f4a296752f71abd2c718b7e6ae0abbe
Jorge Ramirez <joramirez96@gmail.com>
2020-04-15 22:43:49 +0000

    lib

--- lib/pokedex/api.rb
+++ lib/pokedex/api.rb
@@ -3,7 +3,7 @@ class Api
   def self.get_pokemon
       url = "https://pokeapi.co/api/v2/pokemon/"
       response = Net::HTTP.get(URI(url))
+      pokemons = JSON.parse(response)["results"]
       binding.pry
   end 
-
 end 
