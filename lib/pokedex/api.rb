b92bf2c9012e4534b02c43a6c88fd388f75adc84
Jorge Ramirez <joramirez96@gmail.com>
2020-04-16 15:15:25 +0000

    lib

--- lib/pokedex/api.rb
+++ lib/pokedex/api.rb
@@ -4,6 +4,7 @@ class Api
       url = "https://pokeapi.co/api/v2/pokemon/"
       response = Net::HTTP.get(URI(url))
       pokemon = JSON.parse(response)["results"]
-      pokemon.each {|p| pokemon.new(name: p["name"])}
+      pokemon.each {|p| Pokemon.new(name: p["name"])}
+       
   end 
 end 
